a2ecb2fe2d2033d6ddd73f719ef6e99400607c7e
majosalazar <mariaj.salazarf@gmail.com>
2019-04-03 13:34:40 -0300

    fotos de proyecto en 3 de 6

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 btnInicio = () => {
   console.log("hola");
+  sectionPresentacion.style.display = 'block';
+  sectionProyectos.style.diplay = 'block';
+  sectionSobreMi.style.display = 'none';
+  sectionHerramientas.style.diplay = 'block';
+  sectionContacto.style.display = 'block';
+
+}
+
+btnSobreMi = () => {
+  sectionPresentacion.style.display = 'none';
+  sectionProyectos.style.diplay = 'none';
+  sectionSobreMi.style.display = 'block';
+  sectionHerramientas.style.diplay = 'none';
+  sectionContacto.style.display = 'block';
 }
