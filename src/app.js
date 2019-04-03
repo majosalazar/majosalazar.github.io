cb0d9e73645ec4cc99920e062fd6756a54c37e6d
majosalazar <mariaj.salazarf@gmail.com>
2019-04-03 15:39:03 -0300

    botones casi conectados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 btnInicio = () => {
-  console.log("hola");
   sectionPresentacion.style.display = 'block';
   sectionProyectos.style.diplay = 'block';
   sectionSobreMi.style.display = 'none';
@@ -10,8 +9,16 @@ btnInicio = () => {
 
 btnSobreMi = () => {
   sectionPresentacion.style.display = 'none';
-  sectionProyectos.style.diplay = 'none';
+  sectionProyectos.style.display = 'none';
   sectionSobreMi.style.display = 'block';
+  sectionHerramientas.style.diplay = 'none';
+  sectionContacto.style.display = 'block';
+}
+
+btnProyecto = () => {
+  sectionPresentacion.style.display = 'none';
+  sectionProyectos.style.diplay = 'block';
+  sectionSobreMi.style.display = 'none';
   sectionHerramientas.style.diplay = 'none';
   sectionContacto.style.display = 'block';
 }
